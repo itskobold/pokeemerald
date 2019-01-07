9a7cf00b6f90d5bd94b605f60b540cbd5f477b50
itskobold <itskobold@users.noreply.github.com>
2019-01-07 19:53:47 +0000

    tools, resources
    TIME TO GET PORTING I GUESS FUUUUUUUUCK

---
+++ hoenn isles/Documents/keyboard layouts/vanilla keyboards.c
@@ -0,0 +1,27 @@
+//for src/engine/naming_screen.c
+//multiple types of keyboard layouts that can be changed in the options menu
+
+//VANILLA upper page
+//same as vanilla
+    {
+        _(" A B C  D E F    . "),
+        _(" G H I  J K L É  , "),
+        _(" M N O  P Q R S    "),
+        _(" T U V  W X Y Z    "),
+    },
+	
+//VANILLA lower page
+    {
+        _(" a b c  d e f    . "),
+        _(" g h i  j k l é  , "),
+        _(" m n o  p q r s    "),
+        _(" t u v  w x y z    "),
+    },
+	
+//VANILLA others page
+    {
+        _(" 0  1  2  3  4     "),
+        _(" 5  6  7  8  9     "),
+        _(" !  ?  ♂  ♀  /  -  "),
+        _(" …  “  ”  ‘  ’     "),
+    },
